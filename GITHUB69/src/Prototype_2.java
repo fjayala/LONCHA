f157d73ab417d36ce91e371d42307f900b2b664b
fjayala <fjayala@106-17.info2>
2012-05-21 09:20:41 +0200

    mi PRIMER commit

---
+++ GITHUB69/src/Prototype_2.java
@@ -0,0 +1,12 @@
+
+public class Prototype_2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("La PRIMERA modificacion");
+	}
+
+}
