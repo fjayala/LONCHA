b317d36ce4af5ccaa73a77b14b232a22cf0ce36a
fjayala <fjayala@106-17.info2>
2012-05-21 08:53:58 +0200

    Mi PRIMER commit

---
+++ LONCHA/src/maxpayne.java
@@ -0,0 +1,12 @@
+
+public class maxpayne {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("enga ya, battelfield??");
+	}
+
+}
